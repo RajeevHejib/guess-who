aa8b3cab1728799aac8fba4d2207f6b02cd1bb91
Rajeev Hejib <rajeev_hejib@yahoo.com>
2015-12-08 19:26:42 +0000

    implemented gender filter

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -16,12 +16,26 @@ class PlayersController < ApplicationController
     redirect_to '/players'
   end
 
+
+
+  def update
+    @players= Player.all
+    @players.each do |player|
+      player.update(:in_game => false) unless picked_player.gender == player_params["gender"]
+    end
+
+    # @players= Player.where(gender: player_params["gender"])
+    #   @players= Player.all
+    #   # else
+    #   # @players = Player.filter(params.slice(:gender => @gender_param))
+    #   p @players
+    # end
+    redirect_to '/games'
+  end
+
+  private
+
   def player_params
     params.require(:player).permit(:name, :gender, :age, :city, :nationality, :no_of_friends, :marital_status, :image)
   end
-
-  def update
-  end
-
-
 end
